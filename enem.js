a8de84059be64b4ec137de520a16c3274322579c
Victor123-PC <osonobuvictor1994@gmail.com>
2023-12-06 12:50:26 +0100

    test

---
+++ enem.js
@@ -0,0 +1,13 @@
+function addition(num1, num2){
+let sum = num1+num2
+return sum
+}
+console.log(addition(20,40))
+
+
+
+function average (n1,n2, n3){
+    let totalSum = (n1 + n2 +n3)
+    return totalSum/3
+}
+console.log(average(4,6,8))
